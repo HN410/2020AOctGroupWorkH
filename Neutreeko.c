90915f9f2bd827e5b7125669390eb8d56835fe21
HN_410 <sirrook7@gmail.com>
2020-10-25 16:34:33 +0900

    change getMoveList, getMove, add alphabeta

--- Neutreeko.c
+++ Neutreeko.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <math.h>
+#define MAX_DEPTH 4
+#define MAX_EVALUATION_V 10000
+#define BLACK -1
+#define WHITE 1
+
+void getBoard(const char[], const int inputBoard[5][5], int outputBoard[5][5]);
+void getMove(const int board[5][5], char move[4]);
+void getMoveList(const int board[5][5], const int player, char moveList[][4]);
+int getEvaluationValue(const int board[5][5]);
+int hasWon(const int board[5][5]);
+
+
 
 void getBoard(const char input[], const int inputBoard[5][5],
  int outputBoard[5][5]){
@@ -7,12 +20,22 @@ void getBoard(const char input[], const int inputBoard[5][5],
     //outputBoard ...出力する盤面
 }
 
+int alphabeta(const int board[5][5], int turn ,int depth,
+     int alpha, int beta, char move[]){
+         if(depth == MAX_DEPTH) 
+         return getEvaluationValue(board);
+
+
+
+}
+
 void getMove(const int board[5][5], char move[4]){
     //board ... 盤面
     //move ...出力する指し手
+    alphabeta(board, 1, 0, -MAX_EVALUATION_V, MAX_EVALUATION_V, move);
 }
 
-void getMoveList(const int board[5][5], const int player, char moveList[][4]){
+void getMoveList(const int board[5][5], const int player, char moveList[24][4]){
     //board ...盤面
     //player どっちの手番か　黒の番なら-1, 白の番なら1
     //moveList ...可能な指し手のリストの出力
